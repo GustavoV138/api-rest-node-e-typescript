57171d59d6cd7b2306045ead8104f8d4975176e6
Gustavo Vieira <joao.g138+github@gmail.com>
2025-01-20 18:00:19 -0300

    Server.js exclude

--- src/server/Server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.server = void 0;
-var express_1 = require("express");
-var server = (0, express_1.default)();
-exports.server = server;
-server.get('/', function (req, res) {
-    res.send('Olá Dev!');
-});
